0665a5a34e1d61b4316b70cc164172eff98ddfd3
li-meng <li.meng.france@gmail.com>
2021-10-05 00:32:04 +0200

    Create quiz-load-questions.py

---
+++ lambda/quiz-load-questions.py
@@ -0,0 +1,25 @@
+import json
+import boto3
+from time import gmtime, strftime
+
+dynamodb = boto3.resource('dynamodb')
+s3 = boto3.client('s3')
+
+BUCKET_NAME="meng-quiz"
+
+def lambda_handler(event, context):
+    quiz_name=event['quiz_name']
+    table = dynamodb.Table(quiz_name)
+    
+    file_name=quiz_name+".json"
+    data = s3.get_object(Bucket=BUCKET_NAME, Key=file_name)
+    json_content=json.loads(data['Body'].read().decode('utf-8'))
+    
+    print(json_content)
+    response = table.put_item(
+        Item=json_content)
+# return a properly formatted JSON object
+    return {
+        'statusCode': 200,
+        'body': "success"
+    }
